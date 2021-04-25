607808fbcbf51d17da52d1512beeb3d8bbe7beab
Mago <scherer.victor@gmail.com>
2021-04-25 19:31:53 -0300

    esboço fila genérica

--- filaGenerica.py
+++ filaGenerica.py
@@ -1,119 +1,107 @@
-import random
-import argparse
+'''i
+PSEUDO ALGORITMOS FILAS
 
-LOST = 0
+func CHEGADA(fila):
+    incrementa fila
+    se cliente pode ser atendido:
+        destino = sortear(fila.targets)
+        agenda destino
+    agenda chegada de novo cliente na fila
+
+func SAIDA(fila):
+    descrementa fila
+    se a fila não tem servidor disponível (>=):
+        destino = sortear(fila.targets ++ saída)
+        agenda destino
+
+func TANDEM(fila_source, fila_targe):
+    decrementa fila_source
+    se não tem servidor disponível em fila_source(>=):
+        destino = sortear(fila.targets ++ saída)
+        agenda destino
+
+    incrementa fila_target
+    se fila_target tem servidor disponível
+        destino = sortear(fila_target.targets ++ saída)
+        agenda destino
 
 '''
-   o ---->  o===== 1 ----> o======2
-                     '
-                     '---> saída
 
-FUNÇÃO 1
-queue_1.counter -= 1
-    if queue_1.counter >= queue_1.total_servers:
-        if Sort((0,1)) >= psbl_events['SA1']: <---- saída
-            scheduler.schedule('SA1')
-        else:
-            scheduler.scheduler('P12') <--- próxima entrada
-    
-                      --> saída (10%)
-                     '
-                     '
-    o ---->  o===== 1 ----> o======== 2 (80%)
-                     '                        
-                     '-->   o======= 3 (10%) 
-                     
-
-FUNÇÃO 2
-queue_1.counter -= 1
-    if queue_1.counter >= queue_1.total_servers:
-        if Sort((0,1)) >= psbl_events['P12']: <---- entrada 1
-            scheduler.schedule('SA1')
-        elsif Sort((0,1)) >= psbl_events['P13']::
-            scheduler.scheduler('P12') <--- entrada 2
-        else:
-            scheduler.scheduler('SA1') <--- saída
-
-Q1:{(Q2, 0, 10), (Q3, 11,20),  20:Q3, 0:OUT}
-
- Q1 {(q2, 10%), () .. }--> 0 - 10
- Q1 (q3, 10%) --> 10 - 20
- Q1 (out, 80%) --> 20 - 100
-
- z > q2 > y > q3 > x > out
-numero = 0.01
-enquanto( element : dicionário)
-    numero >= element.prob():
-        return
-'''
 
 class Queue:
-    total_servers = 0
-    max_size = 0
-    counter = 0
-
-    def __init__(self, t_serv, s_queue, topo_desc):
-        self.total_servers = t_serv
-        self.max_size = s_queue
-        self.topo = self.create_topo(topo_desc)        
+    name = ""
+    servers = 0
+    minArrival = 0
+    maxArrival = 0
+    minService = 0
+    maxService = 0
     
-    def print_queue(self):
-        print('total servers: ' + str(self.total_servers), end = '\n')
-        print('maximum size: ' + str(self.max_size), end = '\n')
-        print('queue topology: ', end = '')
-        print(self.topo)
+    def Queue(name, servers, minArrival, maxArrival, minService, maxService):
+        self.name = name
+        self.servers = servers
+        self.minArrival = minArrival
+        self.maxArrival = maxArrival
+        self.minService = minService
+        self.maxService = maxService
 
+#estrutura para guardar a topologia das filas, 
+#     a saída da fila representa o resto das probabilidades
+class Topology:
+    topo = {} #source: list (target, probability)
 
+    def AppendSource(source, target, prob):
+        if not source in topo:
+            topo[source] = []
 
-class Network:
-    dic = {}
-    
+        topo[source].append( (targe, prob) )
+    def GetTargets(source):
+        return topo[source]
 
+#pega a próxima fila ou saída de acordo com a probabilidade
+def GetNextDestiny(source_name, list_queues):
+    random_number = sort(0,1)
+    accumulator_1 = 0
+    accumulator_2 = 0
 
-def entrance(queue,)
+    for t_queue in list_queues:
+        name_queue, probability = t_queue
+        accumulator_2 += probability
 
+        if  accumulator_1 <= random_number < accumulator_2:
+            return name_queue
 
+        accumulator_1 += accumulator_2
 
+    return 'exit ' + source_name
 
-def entrance():
-    
-    number = random_number()
-    
-    for next_place in queue.topo:
-        left_lim, right_lim = queue.topo[next_place]
-        if left_lim  <= numero and numero > right_lim:
-            #fazer algo aqui com next_place
+#SCHEDULER DEVE TER UM SOURCE E UM TARGET PARA AVALIAR QUAL DAS 3 FUNÇÕES CHAMAR
+# se source == None e target == queue -> entrance()
+# se source == queue e target == exit -> exit()
+# se source == queue e target == queue -> tandem()
 
-    # key: descricao valor: fila
+def Entrance(queue):
+    if queue.counter <= queue.capacity:
+        queue.counter += 1
+        if queue.counter >= queue.servers:
+            destiny = GetNextDestiny(queue.name, topo.GetTargets(queue.name))
+            Scheduler.schedule(queue.name, destiny, current_time, sort(queue.minService, queue.maxService))
+    Scheduler.schedule(None , queue.name, current_time, sort(queue.minArrival, queue.maxArrival))
 
-q1 = Queue(2,4)
-q2 = Queue(1,5)
+def Exit(queue):
+    queue.counter-=1
+    if queue.counter >= queue.servers:
+        destiny = GetNextDestiny(queue.name, topo.GetTargets(queue.name)) 
+        Scheduler.schedule(queue.name, destiny, current_time, sort(queue.minService, queue.maxService))
 
-q.print_queue()
+def Tandem(queue_source, queue_target)
+    queue_source.counter -=1
 
-dic = {}
-dic[(q1,q2)] = 0.7
+    if queue_source.counter <= queue_source.servers:
+        destiny = GetNextDestiny(queue_source.name, topo.GetTargets(queue_source.name))
+        Scheduler.schedule(queue_source.name, destiny, current_time, sort(queue_source.minService, queue_source.maxService))
+        
 
-
-
-
-
-
-'''
-            --0.2--> O=== 3 --> Saida3
-            '
-            '
-  O=== 1 --0.4--> O==== 2 --> Saida2
-           '
-           '
-           '-> saida1
-
-QUEUE
-    fila1: atributos 
-    fila2: atributos
-
-NETWORK:
-    fila1 : (fila2, 0.4)
-    fila1 : (fila3, 0.4)
-
-'''
+    queue_target.counter += 1
+    if queue_target.counter >= queue_target.servers
+        destiny = GetNextDestiny(queue_target.name, topo.GetTargets(queue_target.name))
+        Scheduler.schedule(queue_target.name, destiny, current_time, sort(queue_target.minService, queue_target.maxService))
